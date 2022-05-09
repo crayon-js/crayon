de5f62f4f02906063ffc9f13d4ef4b44482d7784
Im-Beast <franik.mateusz@gmail.com>
2022-05-09 22:24:51 +0200

    feat(crayon)!: improve typings and prototype functions, greatly improve performance (~12-13x)
    BREAKING CHANGES:
     - `crayonPrototype` has been renamed to `prototype`
     - `clearCache`, `clone`, `instance` functions were deprecated because of new non-mutating nature of crayon.
     - `crayon()` doesn't return Crayon instance anymore because of the same reasons, instead it will return `"undefined"` as calling crayon now requires at least 1 argument.
     - `optimizeStyles` function is now method of `crayon` called `optimize`, it's majorly improved.
     - New crayon typings differ from old ones
    
    Enormous performance gain was caused by many micro-optimizations:
     - using custom `replace` and `replaceAll` function
     - replacing getter that generates crayon instance with value of built crayon after it was acomplished
     - optimizing getting text from arguments given to crayon when only one is given
     - not mutating crayon instance
     - accessing object properties as least as possible, being carefoul with creating new variables
    
    🎉 This performance gain makes crayon faster than `chalk` 🥳

--- src/crayon.ts
+++ src/crayon.ts
@@ -11,9 +11,10 @@ import {
   Style,
   StyleCode,
 } from "./styles.ts";
-import { AnyFunction, capitalize } from "./util.ts";
+import { getNoColor, replaceAll } from "./util.ts";
 
-const hasColor = !Deno.noColor;
+const noColor = getNoColor();
+const hasColor = noColor || noColor == "0" ? true : false;
 
 export interface ColorSupport {
   trueColor: boolean;
@@ -22,75 +23,184 @@ export interface ColorSupport {
   threeBitColor: boolean;
 }
 
-export const colorSupport: ColorSupport = {
+/** An object that expresses how well terminal supports displaying colors */
+export const colorSupport = {
   trueColor: hasColor,
   highColor: hasColor,
   fourBitColor: hasColor,
   threeBitColor: hasColor,
 };
 
-const properties = {
+export type CrayonStyleFunction = (
+  // deno-lint-ignore no-explicit-any
+  ...args: any[]
+) => (StyleCode | string | undefined);
+
+/** Map containing all style functions used by crayon */
+export const functions = new Map<string, CrayonStyleFunction>();
+/** Map containing all styles used by crayon */
+export const styles = new Map<string, StyleCode | string>();
+
+interface CrayonPrototype {
+  colorSupport: ColorSupport;
+  strip(text: string): string;
+  optimize(text: string): string;
+  literal(...text: unknown[]): string;
+}
+
+/** Object, which gets set as the prototype of every generated crayon instance in `generateCrayon()` function */
+export const prototype: CrayonPrototype = {
   get colorSupport(): ColorSupport {
     return colorSupport;
   },
   set colorSupport(value: ColorSupport) {
-    if (hasColor) Object.assign(colorSupport, value);
+    // Keep colorSupport object reference so it doesn't break things
+    Object.assign(colorSupport, value);
   },
   strip(text: string): string {
-    // deno-lint-ignore no-control-regex
-    return text.replace(/\x1b\[[0-9]([0-9])?([0-9])?m/gi, "");
+    return text.replaceAll(/\x1b\[([0-9]|;)+m/gi, "");
+  },
+  optimize(text: string): string {
+    let $text = text;
+    let lenDiff = 0;
+    do {
+      text = $text.replaceAll(/\x1b\[([0-9]|;)+m\x1b\[0m/gi, "\x1b[0m")
+        .replaceAll(
+          /(\x1b\[4([0-9]|;)+m)((\x1b\[([0-9]|;)+m)*(\x1b\[4([0-9]|;)+m))/gi,
+          "$3",
+        ).replaceAll(
+          /(\x1b\[3([0-9]|;)+m)((\x1b\[([0-9]|;)+m)*(\x1b\[3([0-9]|;)+m))/gi,
+          "$3",
+        );
+
+      lenDiff = text.length - $text.length;
+      $text = text;
+    } while (lenDiff !== 0);
+    return text;
+  },
+  literal(): string {
+    throw new Error(
+      "You need to import extension for literal template support to be able to use it",
+    );
   },
 };
 
-export function mapPrototypeFuncs(...funcs: AnyFunction[]) {
-  for (const func of funcs) {
-    Object.defineProperty(properties, func.name, {
-      value(...args: unknown[]) {
-        this.buffer += func(...args, false);
-        const { buffer } = this;
-        this.buffer = "";
-        return generateCrayon(buffer);
-      },
-    });
+/**
+ * Map given style function to `name` and `bgName` keys in `prototype`
+ * When `only` is set to true only `name` key is set and no additional parameters are added when calling function
+ *
+ * @param name – name of the function
+ * @param func – function which gets mapped
+ * @param only – whether to not map bgName func
+ */
+function mapFunc(name: string, func: CrayonStyleFunction, only = false): void {
+  functions.set(name, func);
 
-    Object.defineProperty(properties, `bg${capitalize(func.name)}`, {
-      value(...args: unknown[]) {
-        this.buffer += func(...args, true);
-        const { buffer } = this;
-        this.buffer = "";
-        return generateCrayon(buffer);
+  if (only) {
+    Object.defineProperty(prototype, name, {
+      value(this: Crayon, ...args: unknown[]) {
+        return buildCrayon(this.styleBuffer + (func(...args) ?? ""));
       },
     });
+    return;
   }
+
+  Object.defineProperty(prototype, name, {
+    value(this: Crayon, ...args: unknown[]) {
+      return buildCrayon(this.styleBuffer + (func(...args, false) ?? ""));
+    },
+  });
+
+  const bgName = `bg${name[0].toUpperCase() + name.slice(1)}`;
+  Object.defineProperty(prototype, bgName, {
+    value(this: Crayon, ...args: unknown[]) {
+      return buildCrayon(this.styleBuffer + (func(...args, true) ?? ""));
+    },
+  });
 }
 
-export function mapPrototypeStyles(...maps: Map<string, StyleCode>[]): void {
-  for (const codes of maps) {
-    for (const [name, code] of codes.entries()) {
-      Object.defineProperty(properties, name, {
-        get() {
-          this.buffer += code;
-          const { buffer } = this;
-          this.buffer = "";
-          return generateCrayon(buffer);
-        },
-      });
+/**
+ * Uses `mapFunc` on every given function
+ *  - `func.name` will be considered as `name` parameter
+ *
+ * @param funcs – functions which will get mapped
+ */
+export function mapPrototypeFuncs(...funcs: CrayonStyleFunction[]): void;
+/**
+ * Uses `mapFunc` on every given function
+ *  - map key will be used as `name` parameter
+ *
+ * @param maps – map which functions will get mapped
+ */
+export function mapPrototypeFuncs(
+  ...maps: Map<string, CrayonStyleFunction>[]
+): void;
+export function mapPrototypeFuncs(
+  ...iterable: CrayonStyleFunction[] | Map<string, CrayonStyleFunction>[]
+): void {
+  if (typeof iterable[0] === "function") {
+    for (const func of iterable as CrayonStyleFunction[]) {
+      mapFunc(func.name, func);
+    }
+    return;
+  }
+
+  for (const map of iterable as Map<string, CrayonStyleFunction>[]) {
+    for (const [name, func] of map.entries()) {
+      mapFunc(name, func);
     }
   }
 }
 
+/**
+ * Map given style to `name` key in `prototype`
+ *
+ * @param name – name of the style
+ * @param code – style code which will get mapped
+ */
+function mapStyle(name: string, code: StyleCode): void {
+  Object.defineProperty(prototype, name, {
+    configurable: true,
+    get(this: Crayon) {
+      const builtCrayon = buildCrayon(this.styleBuffer + code);
+      // Instead of building crayon every time property gets accessed
+      // simply replace getter with built crayon instance
+      Object.defineProperty(this, name, { value: builtCrayon });
+      return builtCrayon;
+    },
+  });
+}
+
+/**
+ * Uses `mapStyle` on every given style code
+ *  - map key will be used as `name` parameter
+ * @param maps – map which styles will get mapped
+ */
+export function mapPrototypeStyles(...maps: Map<string, StyleCode>[]): void {
+  for (const map of maps) {
+    for (const [name, code] of map.entries()) {
+      styles.set(name, code);
+      mapStyle(name, code);
+    }
+  }
+}
+
+/** Crayon type which can be easily extended
+ * `C` - literal string type for extending styles
+ * `O` - object which expands
+ */
 export type Crayon<
   C extends string = never,
-  // deno-lint-ignore ban-types
-  O extends Record<string, unknown> = {},
+  O extends Record<string, unknown> = Record<never, never>,
 > =
   & ((...text: unknown[]) => string)
-  & typeof properties
-  // deno-lint-ignore ban-types
-  & (<A extends string = "", B extends Record<string, unknown> = {}>() =>
-    Crayon<C | A, O & B>)
+  & typeof prototype
+  & (<
+    A extends string = "",
+    B extends Record<string, unknown> = Record<never, never>,
+  >() => Crayon<C | A, O & B>)
   & {
-    buffer: string;
+    styleBuffer: string;
     keyword(style: Style): Crayon<C, O>;
     ansi3(code: number): Crayon<C, O>;
     bgAnsi3(code: number): Crayon<C, O>;
@@ -110,29 +220,59 @@ export type Crayon<
   }
   & O;
 
-export function generateCrayon<
-  C extends string = never,
-  // deno-lint-ignore ban-types
-  O extends Record<string, unknown> = {},
->(buffer?: string): Crayon<C, O> {
-  const crayon = ((...text: unknown[]) => {
-    if (!text.length) return crayon;
+export function buildCrayon<T extends Crayon = Crayon>(styleBuffer = ""): T {
+  function crayon(
+    single: unknown & { raw?: boolean },
+    ...many: unknown[]
+  ): T | string {
+    if (single?.raw) return prototype.literal(single, ...many);
 
-    return crayon.buffer +
-      text
-        .join(" ")
-        .replace("\x1b[0m", `\x1b[0m${crayon.buffer}`) +
-      "\x1b[0m";
-  }) as Crayon<C, O>;
+    // Improve performance when only one parameter is given
+    const text = many.length ? `${single} ${many?.join(" ")}` : `${single}`;
+    if (!styleBuffer) return text;
 
-  crayon.buffer = buffer ?? "";
+    return styleBuffer +
+      replaceAll(text, "\x1b[0m\x1b[0m", "\x1b[0m" + styleBuffer) +
+      "\x1b[0m\x1b[0m";
+  }
 
-  Object.setPrototypeOf(crayon, properties);
+  crayon.styleBuffer = styleBuffer;
 
-  return crayon;
+  return Object.setPrototypeOf(crayon, prototype);
 }
 
+// Map default stylings
 mapPrototypeStyles(colors, attributes);
-mapPrototypeFuncs(keyword, ansi3, ansi4, ansi8, rgb, hsl, hex);
+mapPrototypeFuncs(ansi3, ansi4, ansi8, rgb, hsl, hex);
+mapFunc(keyword.name, keyword, true);
 
-export const crayon = generateCrayon();
+/**
+ * 🖍️ Crayon object that's used for styling
+ *  - Call the last property as a function with arguments to get styled string
+ *
+ * @example
+ * ```ts
+ * import { crayon } from ...;
+ *
+ * const info = crayon.bgBlue.white;
+ *
+ * console.log(
+ *  info("You can cache styles like that!"),
+ *  crayon.bgMagenta.black("You can also", "pass multiple arguments!")
+ * );
+ * ```
+ *
+ * When `extensions/literal.ts` is imported literal template styling is possible
+ *
+ * @example
+ * ```ts
+ * import { crayon } from ...;
+ * import ".../src/extensions/literal.ts";
+ *
+ * console.log(
+ *  crayon`{red This text is red {green this is green} and again red}`,
+ *  crayon`{rgb(230,30,20) I love colors by the way}`
+ * );
+ * ```
+ */
+export const crayon = buildCrayon();
