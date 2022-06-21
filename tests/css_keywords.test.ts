a4b3ac99027baf6e27847d7cfd7a234e347c2310
Im-Beast <franik.mateusz@gmail.com>
2022-06-21 16:55:50 +0200

    tests(css_keywords): import remote dependency from deps.ts

--- tests/css_keywords.test.ts
+++ tests/css_keywords.test.ts
@@ -1,5 +1,5 @@
 // Copyright 2022 Im-Beast. All rights reserved. MIT license.
-import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
+import { assertEquals } from "./deps.ts";
 import { crayon, cssKeywords } from "../src/extensions/css_keywords.ts";
 
 Deno.test("Extension: Css Keywords", () => {
