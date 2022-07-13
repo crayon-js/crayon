023c069b394fabffb645006921ae2cb3a35f2712
Im-Beast <franik.mateusz@gmail.com>
2022-07-13 15:01:38 +0200

    feat: exclude crayon size when extension depends on it, refactor the code

--- scripts/gen_badges.ts
+++ scripts/gen_badges.ts
@@ -3,129 +3,117 @@ import { hslToRgb } from "../src/conversions.ts";
 import { emptyDir } from "./deps.ts";
 
 const textDecoder = new TextDecoder();
+const denoExec = Deno.execPath();
 
-async function generateBadge(
-  label: string,
-  message: string,
-  color: string,
-) {
-  return await (await fetch(
-    `https://img.shields.io/static/v1?label=${label}&message=${message}&color=${color}`,
-  )).text();
+interface Badge {
+  label: string;
+  message: string;
+  color: string;
 }
 
-async function coverageBadge() {
-  const taskTest = await Deno.spawn(Deno.execPath(), {
+async function generateBadge(
+  path: string | URL,
+  { label, message, color }: Badge,
+): Promise<void> {
+  const badge = await (await fetch(
+    `https://img.shields.io/static/v1?label=${label}&message=${message}&color=${color}`,
+  )).text();
+
+  await Deno.writeTextFile(path, badge);
+}
+
+async function getTestCoverage(
+  testArgs: string[] = [],
+  coverageArgs: string[] = [],
+): Promise<number> {
+  await emptyDir("./coverage");
+
+  const testChild = Deno.spawnChild(denoExec, {
     args: [
       "task",
       "test",
-      "--coverage=coverage",
+      "--coverage=./coverage",
+      ...testArgs,
     ],
-    stdin: "null",
-    stdout: "piped",
-    stderr: "piped",
   });
 
-  if (!taskTest.status.success) {
-    return generateBadge(
-      "coverage",
-      "unknown",
-      "red",
-    );
-  }
+  await testChild.output();
 
-  const coverage = await Deno.spawn(Deno.execPath(), {
+  const coverageChild = Deno.spawnChild(denoExec, {
     args: [
       "coverage",
-      "coverage/",
+      "./coverage",
+      ...coverageArgs,
     ],
-    stdin: "null",
-    stdout: "piped",
-    stderr: "piped",
   });
 
-  if (!taskTest.status.success) {
-    return generateBadge(
-      "coverage",
-      "unknown",
-      "red",
-    );
-  }
+  const stdout = textDecoder.decode((await coverageChild.output()).stdout);
 
-  const textSdout = await textDecoder.decode(await coverage.stdout);
   let coveredLines = 0;
   let allLines = 0;
-
-  for (
-    const [output] of textSdout.matchAll(/\d+\/\d+/g)
-  ) {
-    const split = output.split("/").map(Number);
-    coveredLines += split[0];
-    allLines += split[1];
+  for (const match of stdout.matchAll(/\((?<covered>\d+)\/(?<all>\d+)\)/g)) {
+    const { covered, all } = match.groups!;
+    coveredLines += +covered;
+    allLines += +all;
   }
 
-  const coveragePercent = (coveredLines / allLines) * 100;
-
-  return generateBadge(
-    "coverage",
-    `${coveragePercent.toFixed(2)}%`,
-    hslToRgb(
-      coveragePercent * 1.2,
-      50,
-      50,
-    ).map((x) => x.toString(16)).join(""),
-  );
+  return (coveredLines / allLines) * 100;
 }
 
-async function sizeBadge(filePath: string) {
-  const info = await Deno.spawn(Deno.execPath(), {
+/** Returns size in kibibytes and whether it has crayon in dependency */
+async function getInfoSize(filePath: string): Promise<[number, boolean]> {
+  const child = Deno.spawnChild(denoExec, {
     args: [
       "info",
       filePath,
       "--json",
     ],
-    stdin: "null",
-    stdout: "piped",
-    stderr: "piped",
   });
 
-  if (!info.status) {
-    return generateBadge(
-      "raw size",
-      "unknown",
-      "red",
-    );
+  const stdout = textDecoder.decode((await child.output()).stdout);
+
+  interface Module {
+    size: number;
+    local: string;
   }
 
-  let json: { modules: { size: number }[] };
-  try {
-    json = JSON.parse(await textDecoder.decode(info.stdout));
-  } catch {
-    return generateBadge(
-      "raw size",
-      "unknown",
-      "red",
-    );
+  const modules = JSON.parse(stdout).modules as Module[];
+  let combinedSize = 0;
+  let hasCrayonAsDependency = false;
+  for (const { size, local } of modules) {
+    combinedSize += size;
+    hasCrayonAsDependency ||= local.includes("mod.ts");
   }
 
-  const size = json.modules.reduce((p, n) => ({
-    size: p.size + n.size,
-  })).size;
-
-  return generateBadge("raw size", `${(size / 1024).toFixed(2)}KB`, "4c76ae");
+  return [combinedSize / 1024, hasCrayonAsDependency];
 }
 
-await emptyDir("docs/badges");
-Deno.writeTextFile("docs/badges/coverage.svg", await coverageBadge());
+const coverage = await getTestCoverage();
+generateBadge("./docs/badges/coverage.svg", {
+  label: "coverage",
+  message: coverage.toFixed(2),
+  color: hslToRgb(coverage * 1.2, 50, 50)
+    .map((value) => value.toString(16))
+    .join(""),
+});
 
-await emptyDir("docs/badges/size");
-Deno.writeTextFile("docs/badges/size/mod.svg", await sizeBadge("mod.ts"));
+const [crayonSize] = await getInfoSize("mod.ts");
+await emptyDir("./docs/badges/size");
+generateBadge("./docs/badges/size/mod.svg", {
+  label: "raw size",
+  message: crayonSize.toFixed(2) + "KiB",
+  color: "4c76ae",
+});
 
-for await (const file of Deno.readDir("src/extensions")) {
-  if (!file.name.endsWith(".ts")) continue;
+for await (const { name } of Deno.readDir("./src/extensions")) {
+  if (!name.endsWith(".ts")) continue;
 
-  Deno.writeTextFile(
-    `docs/badges/size/${file.name.replace(".ts", "")}.svg`,
-    await sizeBadge(`src/extensions/${file.name}`),
-  );
+  let [size, depends] = await getInfoSize(`./src/extensions/${name}`);
+  if (depends) size -= crayonSize;
+
+  generateBadge(`./docs/badges/size/${name.replace(".ts", ".svg")}`, {
+    label: "raw size",
+    message: size.toFixed(2) + "KiB",
+    color: "4c76ae",
+  });
 }
