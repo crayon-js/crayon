6b65052d74b6f91d8f47d8e8e3165bd6ebf933ee
Im-Beast <franik.mateusz@gmail.com>
2024-02-14 22:04:48 +0100

    fix(base): invalid type definition

--- src/base.ts
+++ src/base.ts
@@ -27,12 +27,18 @@ extendMethods({
 type BaseStyles = { [K in Style]: Crayon };
 declare global {
   interface Crayon extends BaseStyles {
-    ansi3(code: number): string;
-    ansi4(code: number): string;
-    ansi8(code: number): string;
-    rgb(r: number, g: number, b: number): string;
-    hsl(h: number, s: number, l: number): string;
-    hex(hex: string): string;
+    ansi3(code: number): Crayon;
+    bgAnsi3(code: number): Crayon;
+    ansi4(code: number): Crayon;
+    bgAnsi4(code: number): Crayon;
+    ansi8(code: number): Crayon;
+    bgAnsi8(code: number): Crayon;
+    rgb(r: number, g: number, b: number): Crayon;
+    bgRgb(r: number, g: number, b: number): Crayon;
+    hsl(h: number, s: number, l: number): Crayon;
+    bgHsl(h: number, s: number, l: number): Crayon;
+    hex(hex: number): Crayon;
+    bgHex(hex: number): Crayon;
     keyword(name: string): string | undefined;
   }
 }
