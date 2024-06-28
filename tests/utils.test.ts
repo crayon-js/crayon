b6e49f9a8d9419fd5825bf1d3afe78aaceb30424
Im-Beast <franik.mateusz@gmail.com>
2024-06-28 17:19:48 +0200

    feat: node support

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,16 +1,17 @@
 // Copyright 2024 Im-Beast. All rights reserved. MIT license.
+import { assertEquals } from "@std/assert";
+import { test } from "@beast/compat";
+
 import { capitalize } from "../src/utils/capitalize.ts";
 import { replace, replaceAll } from "../src/utils/strings.ts";
 
-import { assertEquals } from "@std/assert";
-
-Deno.test("capitalize()", () => {
+test("capitalize()", () => {
   assertEquals(capitalize("dog"), "Dog");
   assertEquals(capitalize("d"), "D");
   assertEquals(capitalize("big fluffy dog"), "Big fluffy dog");
 });
 
-Deno.test("replace()", () => {
+test("replace()", () => {
   assertEquals(replace("Hello world", " world", ""), "Hello");
   assertEquals(replace("Cat likes pets", "Cat", "Dog"), "Dog likes pets");
   assertEquals(replace("", "a", ""), "");
@@ -18,7 +19,7 @@ Deno.test("replace()", () => {
   assertEquals(replace("dog dog dog dog dog", "dog", ""), " dog dog dog dog");
 });
 
-Deno.test("replaceAll()", () => {
+test("replaceAll()", () => {
   assertEquals(replaceAll("Hello worldy world", " world", ""), "Helloy");
   assertEquals(replaceAll("Cat likes pets", "Cat", "Dog"), "Dog likes pets");
   assertEquals(replaceAll("", "a", ""), "");
